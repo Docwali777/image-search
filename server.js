62aefac2e2a6aeb6fab39b5d5b5cb2acb7108947
docwali777 <drgauvin@mgmcare.org>
2017-04-29 18:47:48 -0400

    adding functionality

--- server.js
+++ server.js
@@ -12,62 +12,63 @@ mongoose.connect(dbURl)
 
 const PORT = process.env.PORT || 3000
 
-console.log(PORT)
 app.set('view engine', 'ejs')
 
-
 app.get('/', (req, res) => {
-  res.redirect(`${PORT==3000 ? 'http' : 'https'}://${req.headers.host}/search/flowers?page=2`)
+
+  res.redirect(`${PORT == 3000
+    ? 'http'
+    : 'https'}://${req.headers.host}/search/flowers?page=2`)
 })
 
-app.route('/search/:data')
-.get((req, res) => {
-  console.log(req.params)
-if(req.params.data === 'recent'){
-  imageSchema.find({}, (err, site)=>{
-    if(err){console.log(err)}
-    else{
-      res.render('recent', {
-        site: site.splice(-10, site.length-1)
-      })
-    }
-  })
-}
-else if(req.params.data === 'search'){
-imageSchema.find({}, (err, body)=>{
-  if(err){console.log(err)}
-  else {
-    console.log(body)
-  }
-})
-}
-else
-{
-  console.log(res.statusCode)
-  let pic = new imageSchema({
-    query: req.params.data,
-    info: `https://pixabay.com/api/?key=${API_KEY}&q=${req.params.data}&page=${req.query.page}&per_page=5`,
-    date: new Date().toLocaleString()
-  }).save((err)=>{
-    if(err){console.log(err)}
-  else {
-    imageSchema.find({query: req.params.data}, (err, site)=>{
-fetch(site[site.length-1].info, (err, meta, body)=>{
-  if(err){res.send(statusCode)}
-  else{
-  res.render('search',{
-  data: JSON.parse(body).hits,
-  page: req.query.page,
-  query: req.params.data
-})
-  }
-})
+app.route('/search/:data').get((req, res) => {
 
+  if (req.params.data === 'recent' && req.query.page === undefined) {
+    imageSchema.find({}, (err, site) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.render('recent', {
+          site: site.splice(-10, site.length - 1)
+        })
+      }
+    })
+  } else if (req.params.data === 'search') {
+    imageSchema.find({}, (err, body) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(body)
+      }
+    })
+  } else {
+    let pic = new imageSchema({query: req.params.data, info: `https://pixabay.com/api/?key=${API_KEY}&q=${req.params.data}&page=${req.query.page}&per_page=5`, date: new Date().toLocaleString()}).save((err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        imageSchema.find({
+          query: req.params.data
+        }, (err, site) => {
+          fetch(site[site.length - 1].info, (err, meta, body) => {
+            if (err) {
+              console.log(statusCode)
+            } else {
+              res.render('search', {
+                data: JSON.parse(body).hits,
+                page: req.query.page,
+                query: req.params.data,
+                port: req.headers.host,
+                protocol: PORT == 3000 ? 'http': 'https',
+                path: req.path,
+                query: JSON.stringify(req.query)
+              })
+            }
+          })
+
+        })
+      }
     })
   }
-  })
-}
-
 
 })
 
